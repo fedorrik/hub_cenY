c18b1bff78ce2cd818531a55af8bd31744a2a8a4
fedor <fedorrik1@gmail.com>
2022-07-17 18:19:47 +0300

    add strand track

--- RP-11_cenY/bed/bed2bb.sh
+++ RP-11_cenY/bed/bed2bb.sh
@@ -2,7 +2,7 @@
 track=`echo $1 | awk '{split($0, name, "."); print name[1]}'`
 python bed4track.py $track.bed > bed4track.bed
 
-/home/fedor/Programs/my/fastools/fastools seqlen $2 > chrom.sizes
-bedToBigBed bed4track.bed chrom.sizes $track.bb
+/home/fedor/Programs/my/fastools/fastools seqlen ../rp11.cenY.fa > chrom.sizes
+~/Programs/bedToBigBed bed4track.bed chrom.sizes $track.bb
 rm chrom.sizes
 rm bed4track.bed
--- RP-11_cenY/bed/bed4track.py
+++ RP-11_cenY/bed/bed4track.py
@@ -6,7 +6,7 @@ out_bed = []
 for line in read_tsv(argv[1]):
     if line[0] == 'track':
         continue
-    score = str(int(round(float(line[4])/10, 0))) #/10 for horhap
+    score = str(int(round(float(line[4]), 0))) #/10 for horhap
     name = line[3]
     if len(name) > 254:
         name = 'too_long'
