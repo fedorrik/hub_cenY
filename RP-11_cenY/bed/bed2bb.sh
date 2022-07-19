a16ddfb4542efd763b970066e0fc7ddb0a1aa19d
Fedor <fedorrik1@gmail.com>
2022-07-20 00:15:35 +0300

    more genomes

--- RP-11_cenY/bed/bed2bb.sh
+++ RP-11_cenY/bed/bed2bb.sh
@@ -3,6 +3,6 @@ track=`echo $1 | awk '{split($0, name, "."); print name[1]}'`
 python bed4track.py $track.bed > bed4track.bed
 
 /home/fedor/Programs/my/fastools/fastools seqlen ../rp11.cenY.fa > chrom.sizes
-~/Programs/bedToBigBed bed4track.bed chrom.sizes $track.bb
+~/bin/bedToBigBed bed4track.bed chrom.sizes $track.bb
 rm chrom.sizes
 rm bed4track.bed
