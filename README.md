6594e35ebe39de4a09926b1991fe79cf6f77d1ea
fedorrik <fedorrik1@gmail.com>
2023-01-13 10:30:33 +0100

    add 3 HGSVC genomes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # hub_RP-11
+
+UCSC browser hub for assemblies of chrY's centromere regions (cenY):
+
+- RP-11.cenY
+- HG01358.cenY
+- HG01243.cenY
+- HG03579.cenY
+- HG00621.cenY
+- HG01109.cenY
+- HG02055.cenY
+
+and new HGSVC chrY assemblies:
+
+- HC02666.chrY
+- HG00358.chrY
+- HG01890.chrY
--- RP-11_cenY/scripts/bed2bb.sh
+++ RP-11_cenY/scripts/bed2bb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # fastools is my scripts for workong with fasta files https://github.com/fedorrik/fastools
-~/Programs/my/fastools/fastools seqlen ../rp11.cenY.fa > chrom.sizes
+~/Programs/my/fastools/fastools seqlen ../../../rp11.cenY.fa > chrom.sizes
 for bed in ../bed/*.bed
 do
   track=`echo $bed | awk '{split($0, name, "/"); print name[3]}' | awk '{split($0, name, "."); print name[1]}'`
-  python bed4track.py ../bed/$track.bed > bed4track.bed
+  python3 bed4track.py ../bed/$track.bed | bedtools sort > bed4track.bed
   bedToBigBed bed4track.bed chrom.sizes ../bb/$track.bb
   rm bed4track.bed
 done
